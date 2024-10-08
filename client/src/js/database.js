856d8143aa21e5a3cb3395972a6e1d35d6b472d1
Zain Ul Abid <bajwazain121@outlook.com>
2024-10-07 22:11:56 -0400

    Updated Code for sw

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,22 +1,22 @@
 import { openDB } from "idb";
 
-const initdb = async () =>
-  openDB("jate", 1, {
+const initdb = async () => {
+  return openDB(jate, 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
-
+};
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const txtEditorDb = await openDB("txtEditor", 1);
-  const tx = txtEditorDb.transaction("txtEditor", "readwrite");
-  const store = tx.objectStore("txtEditor");
+  const txtEditorDb = await openDB("jate", 1);
+  const tx = txtEditorDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log("Data saved to the database", result);
@@ -24,8 +24,8 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const txtEditorDb = await openDB("txtEditor", "readonly");
-  const store = transVar.objectStore("txtEditor");
+  const txtEditorDb = await openDB("jate", "1");
+  const store = txtEditorDb.objectStore("jate");
   const request = store.get(1);
   const result = await request;
   result
